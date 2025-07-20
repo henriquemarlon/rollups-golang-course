7b17d4c17af707dd3080342b0c48d14cea7230de
Henrique Santos <henriquemarloncs@gmail.com>
2025-07-20 23:45:56 +0100

    udpate tribes

--- src/05/contracts/src/token/ERC721/NFT.sol
+++ src/05/contracts/src/token/ERC721/NFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
+
 pragma solidity ^0.8.27;
 
 import {Ownable} from "@openzeppelin-contracts-5.2.0/access/Ownable.sol";
